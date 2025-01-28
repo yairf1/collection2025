04bd48c808d78a94efa1cda4f8de16e37118bfd9
yairf1 <128710549+yairf1@users.noreply.github.com>
2025-01-28 11:18:17 +0200

    bug fix with id

--- public/adminPage/admin.js
+++ public/adminPage/admin.js
@@ -143,7 +143,7 @@ async function primeCard(order) {
             </svg>
           </button>
 
-          <button type="button" class="btn btn-secondary me-1 ms-2" id="toggleButton${order.orderId}" onclick="showDetails('${order.orderId}')" data-bs-toggle="tooltip" data-bs-placement="top" title="הצג / הסתר פרטים">
+          <button type="button" class="btn btn-secondary me-1 ms-2" id="toggleButton${order._id}" onclick="showDetails('${order._id}')" data-bs-toggle="tooltip" data-bs-placement="top" title="הצג / הסתר פרטים">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
               <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659"/>
             </svg>
@@ -163,7 +163,7 @@ async function primeCard(order) {
           </button>
         </div>
       </div>
-      <div id="products-${order.orderId}" class="products-container" style="display: none;">
+      <div id="products-${order._id}" class="products-container" style="display: none;">
         ${productCards.join('')}
       </div>
     </div>`;
